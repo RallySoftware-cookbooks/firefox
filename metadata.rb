40ff9aed3456067c2ea67a053e2a690b225da680
Russ Teabeault and Matt Farrar <rteabeault@rallydev.com>
2013-10-10 11:09:13 -0600

    Updating to use VERSION file.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'rteabeault@rallydev.com'
 license          'MIT'
 description      'Installs/Configures firefox'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
 depends 'ark', '~> 0.3'
